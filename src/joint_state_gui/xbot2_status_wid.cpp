5c10f416370021ee1d5abfc1eb130fecb51ed625
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-01-22 19:03:49 +0100

    Revert "update gui with robot bringup procedure"
    This reverts commit 1f5450123cc5f838df4eb0858381cae2c2c513bc.

--- src/joint_state_gui/xbot2_status_wid.cpp
+++ src/joint_state_gui/xbot2_status_wid.cpp
@@ -1,5 +1,4 @@
 #include "xbot2_status_wid.h"
-#include "bringup_widget.h"
 
 #include <QUiLoader>
 #include <QFile>
@@ -9,13 +8,10 @@
 #include <QMouseEvent>
 #include <QFrame>
 #include <QMessageBox>
-#include <QMenu>
-#include <QInputDialog>
 
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
 #include <std_srvs/Trigger.h>
-#include <xbot_msgs/GetPluginList.h>
 
 void xbot2_status_widget_qrc_init()
 {
@@ -39,20 +35,6 @@ QWidget * LoadUiFile(QWidget * parent)
     return formWidget;
 }
 
-QWidget * LoadUiFileBringup(QWidget * parent)
-{
-    xbot2_status_widget_qrc_init();
-    QUiLoader loader;
-
-    QFile file(":/bringup.ui");
-    file.open(QFile::ReadOnly);
-
-    QWidget *formWidget = loader.load(&file, parent);
-    file.close();
-
-    return formWidget;
-}
-
 }
 
 XBot2StatusWidget::XBot2StatusWidget(QWidget* parent):
@@ -65,7 +47,7 @@ XBot2StatusWidget::XBot2StatusWidget(QWidget* parent):
     setLayout(layout);
 
     // status label
-    _status_label = findChild<QLabel*>("statusLabelXbot");
+    _status_label = findChild<QLabel*>("statusLabel");
     _status_label->setToolTip("xbot2 process status from topic 'xbotcore/status'");
     _status_label->setText("Inactive");
 
@@ -81,6 +63,7 @@ XBot2StatusWidget::XBot2StatusWidget(QWidget* parent):
 
     // cmd button
     _cmd_button = findChild<QPushButton*>("cmdBtn");
+    _cmd_button->setStyleSheet("font-size: 18px");
     _cmd_button->setToolTip("Start/stop xbot2 process "
                             "(needs xbot2-launcher daemon running)");
 
@@ -89,108 +72,109 @@ XBot2StatusWidget::XBot2StatusWidget(QWidget* parent):
 
     connect(_cmd_button, &QPushButton::released,
             [this]()
-    {
-        if(_cmd_button->text() == "Start")
-        {
-            std_srvs::Trigger srv;
-            if(!_srv_start.waitForExistence(ros::Duration(1.0)))
             {
-                QMessageBox msgBox;
-                msgBox.setText("Start service is offline, make sure "
-                               "xbot2-launcher daemon is up and running");
-                msgBox.exec();
-                return;
-            }
+                if(_cmd_button->text() == "Start")
+                {
+                    std_srvs::Trigger srv;
+                    if(!_srv_start.waitForExistence(ros::Duration(1.0)))
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Start service is offline, make sure "
+                                       "xbot2-launcher daemon is up and running");
+                        msgBox.exec();
+                        return;
+                    }
 
-            if(!_srv_start.call(srv))
-            {
-                QMessageBox msgBox;
-                msgBox.setText("Start service failed, make sure "
-                               "xbot2-launcher daemon is up and running");
-                msgBox.exec();
-                return;
-            }
+                    if(!_srv_start.call(srv))
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Start service failed, make sure "
+                                       "xbot2-launcher daemon is up and running");
+                        msgBox.exec();
+                        return;
+                    }
 
-            if(!srv.response.success)
-            {
-                QMessageBox msgBox;
-                msgBox.setText("Start service returned false: " +
-                               QString::fromStdString(srv.response.message));
-                msgBox.exec();
-                return;
-            }
+                    if(!srv.response.success)
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Start service returned false: " +
+                                       QString::fromStdString(srv.response.message));
+                        msgBox.exec();
+                        return;
+                    }
 
-            emit xbot2Started();
+                    emit xbot2Started();
 
-        }
-        else if(_cmd_button->text() == "Stop")
-        {
-            std_srvs::Trigger srv;
-            if(!_srv_stop.waitForExistence(ros::Duration(1.0)))
-            {
-                QMessageBox msgBox;
-                msgBox.setText("Stop service is offline, make sure "
-                               "xbot2-launcher daemon is up and running");
-                msgBox.exec();
-                return;
-            }
+                }
+                else if(_cmd_button->text() == "Stop")
+                {
+                    std_srvs::Trigger srv;
+                    if(!_srv_stop.waitForExistence(ros::Duration(1.0)))
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Stop service is offline, make sure "
+                                       "xbot2-launcher daemon is up and running");
+                        msgBox.exec();
+                        return;
+                    }
 
-            if(!_srv_stop.call(srv))
-            {
-                QMessageBox msgBox;
-                msgBox.setText("Stop service failed, make sure "
-                               "xbot2-launcher daemon is up and running");
-                msgBox.exec();
-                return;
-            }
+                    if(!_srv_stop.call(srv))
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Stop service failed, make sure "
+                                       "xbot2-launcher daemon is up and running");
+                        msgBox.exec();
+                        return;
+                    }
 
-            if(!srv.response.success)
-            {
-                QMessageBox msgBox;
-                msgBox.setText("Stop service returned false: " +
-                               QString::fromStdString(srv.response.message));
-                msgBox.exec();
-                return;
-            }
-        }
-    });
+                    if(!srv.response.success)
+                    {
+                        QMessageBox msgBox;
+                        msgBox.setText("Stop service returned false: " +
+                                       QString::fromStdString(srv.response.message));
+                        msgBox.exec();
+                        return;
+                    }
+                }
+            });
 
     // kill btn
     auto kill_btn = findChild<QPushButton*>("killBtn");
+    kill_btn->setStyleSheet("font-size: 18px");
     connect(kill_btn, &QPushButton::released,
             [this]()
-    {
-        auto cli = _nh.serviceClient<std_srvs::Trigger>("d/kill");
+            {
+                auto cli = _nh.serviceClient<std_srvs::Trigger>("d/kill");
 
-        std_srvs::Trigger srv;
-        if(!cli.waitForExistence(ros::Duration(1.0)))
-        {
-            QMessageBox msgBox;
-            msgBox.setText("Kill service is offline, make sure "
-                           "xbot2-launcher daemon is up and running");
-            msgBox.exec();
-            return;
-        }
+                std_srvs::Trigger srv;
+                if(!cli.waitForExistence(ros::Duration(1.0)))
+                {
+                    QMessageBox msgBox;
+                    msgBox.setText("Kill service is offline, make sure "
+                                   "xbot2-launcher daemon is up and running");
+                    msgBox.exec();
+                    return;
+                }
 
-        if(!cli.call(srv))
-        {
-            QMessageBox msgBox;
-            msgBox.setText("Kill service failed, make sure "
-                           "xbot2-launcher daemon is up and running");
-            msgBox.exec();
-            return;
-        }
+                if(!cli.call(srv))
+                {
+                    QMessageBox msgBox;
+                    msgBox.setText("Kill service failed, make sure "
+                                   "xbot2-launcher daemon is up and running");
+                    msgBox.exec();
+                    return;
+                }
 
-        if(!srv.response.success)
-        {
-            QMessageBox msgBox;
-            msgBox.setText("Kill service returned false: \n" +
-                           QString::fromStdString(srv.response.message));
-            msgBox.exec();
-            return;
-        }
+                if(!srv.response.success)
+                {
+                    QMessageBox msgBox;
+                    msgBox.setText("Kill service returned false: \n" +
+                                   QString::fromStdString(srv.response.message));
+                    msgBox.exec();
+                    return;
+                }
 
-    });
+            });
 
     // vbatt lcd
     auto on_vbatt_recv = [this](const std_msgs::Float32ConstPtr& msg)
@@ -216,77 +200,7 @@ XBot2StatusWidget::XBot2StatusWidget(QWidget* parent):
 
     findChild<QLabel*>("voltLabel")->setStyleSheet("font-size: 24px");
 
-    // bringup
-    auto bringupBtn = findChild<QPushButton*>("bringupBtn");
-    auto bringupBtnClicked = [this]()
-    {
-        auto bringupWid = new bringup_widget(this);
-
-//        auto geom = bringupWid->geometry();
-//        geom.setTopLeft(event->globalPos());
-//        _load_plugin_wid->setGeometry(geom);
-
-        bringupWid->exec();
-    };
-    connect(bringupBtn, &QPushButton::released, bringupBtnClicked);
-
     handleStatusLabel();
-}
-
-void XBot2StatusWidget::contextMenuEvent(QContextMenuEvent* event)
-{
-    QMenu menu(this);
-
-    auto * load_action = new QAction("Select profile", this);
-
-    connect(load_action, &QAction::triggered,
-            [this, event]()
-    {
-        QList<QString> items;
-
-        auto cli = _nh.serviceClient<xbot_msgs::GetPluginList>("d/get_hw_types");
-
-        xbot_msgs::GetPluginList srv;
-        if(!cli.waitForExistence(ros::Duration(1.0)))
-        {
-            QMessageBox msgBox;
-            msgBox.setText("get_hw_types service is offline, make sure "
-                           "xbot2-launcher daemon is up and running");
-            msgBox.exec();
-            return;
-        }
-
-        if(!cli.call(srv))
-        {
-            QMessageBox msgBox;
-            msgBox.setText("get_hw_types service failed, make sure "
-                           "xbot2-launcher daemon is up and running");
-            msgBox.exec();
-            return;
-        }
-
-        items.append("Auto");
-        std::transform(srv.response.plugins.begin(),
-                       srv.response.plugins.end(),
-                       std::back_inserter(items),
-                       QString::fromStdString);
-
-
-        auto i = QInputDialog::getItem(this,
-                                       "Select profile",
-                                       "Profiles",
-                                       items);
-
-        if(!i.isEmpty())
-        {
-            _hw_type = i.toStdString();
-        }
-
-    });
-
-    menu.addAction(load_action);
-
-    menu.exec(event->globalPos());
 }
 
 void XBot2StatusWidget::update()
@@ -303,29 +217,29 @@ void XBot2StatusWidget::handleStatusLabel()
     if(_status_label->text() == "Running")
     {
         _status_label->setStyleSheet(
-                    "font-size: 18px; "
-                    "background-color: #99ff99;"
-                    "border-radius: 4px;"
-                    "color: green");
+            "font-size: 18px; "
+            "background-color: #99ff99;"
+            "border-radius: 4px;"
+            "color: green");
 
         _cmd_button->setText("Stop");
     }
     else if(_status_label->text() == "Initializing")
     {
         _status_label->setStyleSheet(
-                    "font-size: 18px; "
-                    "background-color: #ff9248;"
-                    "border-radius: 4px;"
-                    "color: black");
+            "font-size: 18px; "
+            "background-color: #ff9248;"
+            "border-radius: 4px;"
+            "color: black");
 
         _cmd_button->setText("Stop");
     }
     else {
         _status_label->setStyleSheet(
-                    "font-size: 18px; "
-                    "background-color: #D3D3D3;"
-                    "border-radius: 4px;"
-                    "color: gray");
+            "font-size: 18px; "
+            "background-color: #D3D3D3;"
+            "border-radius: 4px;"
+            "color: gray");
 
         _cmd_button->setText("Start");
     }

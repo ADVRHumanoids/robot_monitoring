bb0aea8bb1bbc5c35184f93dccacdf9fbc9ec4e0
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-02-20 16:43:31 +0100

    updated softhand plugin

--- src/softhand/softhand_widget.cpp
+++ src/softhand/softhand_widget.cpp
@@ -5,6 +5,7 @@
 #include <QtUiTools>
 
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Float64.h>
 
 #include <eigen3/Eigen/Dense>
 #include <tf2/buffer_core.h>
@@ -35,11 +36,32 @@ QWidget * LoadUiFile(QWidget * parent)
     return formWidget;
 }
 
+QWidget * LoadUiFileHand(QWidget * parent)
+{
+    softhand_widget_qrc_init();
+
+    QUiLoader loader;
+
+    QFile file(":/hand_state.ui");
+    file.open(QFile::ReadOnly);
+
+    QWidget *formWidget = loader.load(&file, parent);
+    file.close();
+
+    return formWidget;
+}
+
+
 }
 
 
 void SofthandWidget::setProgressBarValue(QProgressBar* pb, double value, double max)
 {
+    if (!pb->isEnabled())
+    {
+        pb->setEnabled(true);
+    }
+
     pb->setValue(100*fabs(value)/max);
     pb->setFormat(QString("%1").arg(value, 5, 'f', 1));
 }
@@ -83,11 +105,16 @@ bool SofthandWidget::init(CustomQtWidget::Args& args)
                 continue;
             }
 
+            if (name.contains("/motor_state"))
+            {
+                continue;
+            }
+
             QRegularExpression regex("^/(.+?)/");
             QString ns_name = regex.match(name).captured(1);
 
-            std::cout << "adding " << name.toStdString() << " to: " << ns_name.toStdString() << std::endl;
             softhand_names[ns_name].append(name);
+
 
         }
     }
@@ -104,105 +131,195 @@ bool SofthandWidget::init(CustomQtWidget::Args& args)
         setEnabled(false);
     }
 
+    std::vector<std::string> fingers = {"index", "little", "middle", "ring", "thumb"};
+    std::vector<std::string> finger_joints = {"knuckle", "proximal", "middle", "distal"};
+    std::vector<std::string> thumb_joints = {"knuckle", "proximal", "middle"};
+
+    std::map<std::string, std::vector<std::string>> hand_map;
+
+    for (std::string finger : fingers)
+    {
+        if (finger != "thumb")
+        {
+            hand_map[finger] = finger_joints;
+        }
+        else
+        {
+            hand_map[finger] = thumb_joints;
+        }
+
+    }
+
     // softhand selection
     _select_softhand = findChild<QComboBox*>("softhandCombo");
 
-    auto createProgressBar = [this, softhand_names](QLayout * layout, QString hand_name)
-    {
+//    auto createProgressBar = [this, softhand_names](QLayout * layout, QString hand_name)
+//    {
 
-//         spawn a progress bar for each finger
-        const auto& finger_list = softhand_names[hand_name];
+//        // spawn a progress bar for each finger
+//        const auto& finger_list = softhand_names[hand_name];
 
-        for(auto topic : finger_list)
-        {
-            QString strip_name = topic;
-            strip_name.replace("/" + hand_name, "");
+//        for(auto topic : finger_list)
+//        {
 
-            std::cout << "Finger detected: " << strip_name.toStdString() << std::endl;
+//            QString strip_name = topic;
+//            strip_name.replace("/" + hand_name, "");
 
-            QGroupBox *fingerBox = new QGroupBox(strip_name);
+//            // I should expect the phalanges to be fixed
 
-            QGridLayout *grid = new QGridLayout;
+//            QGroupBox *fingerBox = new QGroupBox(strip_name);
 
-            QLabel *pos_name = new QLabel("Position");
-            QLabel *effort_name = new QLabel("Effort");
+//            QGridLayout *grid = new QGridLayout;
 
-            QProgressBar *positionProgBar = new QProgressBar();
-            QProgressBar *effortProgBar = new QProgressBar();
+//            QLabel *pos_name = new QLabel("Position");
+//            QLabel *effort_name = new QLabel("Effort");
 
-            grid->addWidget(pos_name, 0, 0);
-            grid->addWidget(positionProgBar, 0, 1);
+//            QProgressBar *positionProgBar = new QProgressBar();
+//            QProgressBar *effortProgBar = new QProgressBar();
 
-            grid->addWidget(effort_name, 1, 0);
-            grid->addWidget(effortProgBar, 1, 1);
+//            grid->addWidget(pos_name, 0, 0);
+//            grid->addWidget(positionProgBar, 0, 1);
 
-            fingerBox->setLayout(grid);
-            layout->addWidget(fingerBox);
+//            grid->addWidget(effort_name, 1, 0);
+//            grid->addWidget(effortProgBar, 1, 1);
 
-            // receive state
-            auto cb = [this, hand_name, topic, positionProgBar, effortProgBar]
-                    (const sensor_msgs::JointStateConstPtr& msg)
-            {
-                if(_select_softhand->currentText() != hand_name)
-                {
-                    return;
-                }
+//            fingerBox->setLayout(grid);
+//            layout->addWidget(fingerBox);
 
-                _link_name_map.at(hand_name) = QString::fromStdString(msg->header.frame_id);
+//            // receive state
+//            auto cb = [this, hand_name, topic, positionProgBar, effortProgBar]
+//                    (const sensor_msgs::JointStateConstPtr& msg)
+//            {
+//                if(_select_softhand->currentText() != hand_name)
+//                {
+//                    return;
+//                }
 
-                setProgressBarValue(positionProgBar, msg->position[0], 1.0);
-                setProgressBarValue(effortProgBar, msg->effort[0], 50);
+//                _link_name_map.at(hand_name) = QString::fromStdString(msg->header.frame_id);
 
+//                setProgressBarValue(positionProgBar, msg->position[0], 1.0);
+//                setProgressBarValue(effortProgBar, msg->effort[0], 10);
+
+
+
+//                for (std::string name : msg->name)
+//                {
+
+//                }
 //                auto link_name = QString::fromStdString(msg->header.frame_id);
 
 //                emit pointAdded(link_name + "/effort",
 //                                QPointF(msg->header.stamp.toSec(),
 //                                        msg->effort[0]));
 
+//            };
+
+//            auto sub = _nh.subscribe<sensor_msgs::JointState>(
+//                        topic.toStdString(),
+//                        1,
+//                        cb);
+
+//            _subs.push_back(sub);
+//            _link_name_map[hand_name] = "";
+//        }
+//    };
+
+
+    auto frame_states = findChild<QFrame*>("frameStates");
+    auto frame_states_layout = new QGridLayout;
+    frame_states->setLayout(frame_states_layout);
+
+    std::map<std::string, std::vector<QWidget*>> finger_widgets;
+
+    int row_i = 0;
+    for (auto finger : hand_map)
+    {
+        auto finger_name = finger.first + "_state";
+        QLabel *finger_label = new QLabel(QString::fromStdString(finger_name));
+        finger_label->setStyleSheet("font-weight: bold; ");
+
+
+        int col_i = 0;
+        for (auto elem : finger.second)
+        {
+            auto wid_joints = LoadUiFileHand(this);
+            finger_widgets[finger_name].push_back(wid_joints);
+
+            auto frame_states = wid_joints->findChild<QLabel*>("jointName");
+            frame_states->setText(QString::fromStdString(elem));
+
+            frame_states_layout->addWidget(wid_joints, row_i+1, col_i);
+            col_i++;
+        }
+
+        frame_states_layout->addWidget(finger_label, row_i, 0, 1, col_i+1, Qt::AlignCenter);
+
+        QFrame *line_1 = new QFrame();
+        line_1->setObjectName(QString::fromUtf8("line"));
+        line_1->setGeometry(QRect(320, 150, 118, 3));
+        line_1->setFrameShape(QFrame::HLine);
+        line_1->setFrameShadow(QFrame::Sunken);
+
+        frame_states_layout->addWidget(line_1, row_i+2, 0, 1, col_i+1);
+
+
+        row_i = row_i+3;
+    }
+
+    const auto& m1 = softhand_names.toStdMap();
+    for(auto softhand_name : m1)
+    {
+        for (auto finger_topic : softhand_name.second)
+        {
+            auto cb = [this, finger_topic, softhand_name, finger_widgets]
+                    (const sensor_msgs::JointStateConstPtr& msg)
+            {
+                if(_select_softhand->currentText() != softhand_name.first)
+                {
+                    return;
+                }
+
+                QString strip_name = finger_topic;
+
+                strip_name.replace("/" + softhand_name.first + "/", "");
+                std::string name_finger = strip_name.toStdString();
+
+                _link_name_map.at(finger_topic) = QString::fromStdString(msg->header.frame_id);
+
+                if (finger_widgets.find(name_finger) == finger_widgets.end())
+                {
+                    std::cout << name_finger << " not considered in GUI." << std::endl;
+                }
+                else
+                {
+                    for (int wid_id = 0; wid_id < finger_widgets.at(name_finger).size(); wid_id++)
+                    {
+                        auto pb = finger_widgets.at(name_finger)[wid_id]->findChild<QProgressBar*>("jointProgressBar");
+                        setProgressBarValue(pb, msg->position[wid_id], 1.0);
+                    }
+                }
+
             };
 
-            std::cout <<topic.toStdString() << std::endl;
             auto sub = _nh.subscribe<sensor_msgs::JointState>(
-                        topic.toStdString(),
+                        finger_topic.toStdString(),
                         1,
                         cb);
 
             _subs.push_back(sub);
-            _link_name_map[hand_name] = "";
+            _link_name_map[finger_topic] = "";
 
         }
-    };
 
+        _select_softhand->addItem(softhand_name.first);
 
-    // for each softhand create a tab
-    const auto& m1 = softhand_names.toStdMap();
-    for (auto softhand : m1)
-    {
-        _select_softhand->addItem(softhand.first);
+        auto command_topic_name = "/" + softhand_name.first + "/synergy_command";
+        auto pub = _nh.advertise<std_msgs::Float64>(command_topic_name.toStdString(),
+                                                     1);
+
+        _pub_map[command_topic_name] = pub;
+
     }
-
-
-
-    // get main frame to add progress bars
-    auto frame_states = findChild<QFrame*>("frameStates");
-    auto frame_states_layout = new QVBoxLayout;
-    frame_states->setLayout(frame_states_layout);
-    // initialize layout
-    createProgressBar(frame_states_layout, _select_softhand->currentText());
-
-    connect(_select_softhand, &QComboBox::currentTextChanged,
-            [this, frame_states, createProgressBar](QString hand_name)
-    {
-
-        auto current_layout = frame_states->layout();
-        clearLayout(current_layout);
-
-        createProgressBar(current_layout, hand_name);
-    });
-
-
-//    // stupid hack to trigger text change
-//    _select_softhand->setCurrentIndex(1);
 
     // command
     auto cmdSlider = findChild<QSlider*>("cmdSlider");
@@ -233,27 +350,18 @@ bool SofthandWidget::init(CustomQtWidget::Args& args)
     connect(cmdSendBtn, &QPushButton::released,
             [this, cmdSpinBox]()
     {
-        ros::Publisher pub;
-
-        auto command_topic_name = _select_softhand->currentText() + "/synergy_command";
+        auto command_topic_name = "/" + _select_softhand->currentText() + "/synergy_command";
         auto cmd_topic = _pub_map.find(command_topic_name);
 
         if(cmd_topic == _pub_map.end())
         {
-            std::cout << "advertised " << command_topic_name.toStdString() << "\n";
-            pub = _nh.advertise<sensor_msgs::JointState>(command_topic_name.toStdString(),
-                                                         1);
-//            _pub_map[state_topic] = pub;
-        }
-        else
-        {
-            pub = cmd_topic->second;
+            std::cout << "Topic: '" + command_topic_name.toStdString() + "' not found." << std::endl;
+            return;
         }
 
-        sensor_msgs::JointState msg;
-        msg.header.stamp = ros::Time::now();
-        msg.position = {cmdSpinBox->value()};
-        pub.publish(msg);
+        std_msgs::Float64 msg;
+        msg.data = {cmdSpinBox->value()};
+        cmd_topic->second.publish(msg);
 
     });
 

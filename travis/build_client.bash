14354af69207d3289bdd1ba368ae2378da5339be
Arturo Laurenzi <arturo.laurenzi@iit.it>
2023-11-23 19:17:23 +0100

    Update build_client.bash

--- travis/build_client.bash
+++ travis/build_client.bash
@@ -6,7 +6,7 @@ cp -r $PWD travis/docker/context/robot_monitoring
 
 docker build travis/docker -t travis_build_image
 
-docker run --rm  -u $(id -u) \
+docker run --rm \
  -v $PWD/build_output:/home/user/build_output \
  travis_build_image bash /home/user/build.bash
 

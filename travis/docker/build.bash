414e8024b4ec484f4ab7548a8211cad185c05663
Arturo Laurenzi <arturo.laurenzi@iit.it>
2024-10-15 14:43:39 +0200

    try qt 6.8 lts

--- travis/docker/build.bash
+++ travis/docker/build.bash
@@ -10,7 +10,7 @@ export ANDROID_NDK_ROOT=$ANDROID_SDK_ROOT/ndk/25.1.8937393
 
 # android
 mkdir build && cd build
-~/Qt/6.6.0/android_arm64_v8a/bin/qt-cmake -DCMAKE_BUILD_TYPE=Release ../robot_monitoring
+~/Qt/$QT_VER/android_arm64_v8a/bin/qt-cmake -DCMAKE_BUILD_TYPE=Release ../robot_monitoring
 make -j $(nproc)
 cp xbot2_gui/android-build/build/outputs/apk/release/android-build-release-unsigned.apk /home/user/build_output/xbot2_gui_client_android_arm64_v8a.apk
 
@@ -21,7 +21,7 @@ rm -rf build
 export PATH="$HOME/Qt/Tools/CMake/bin:$PATH"
 
 mkdir build && cd build
-~/Qt/6.6.0/gcc_64/bin/qt-cmake -DCMAKE_INSTALL_PREFIX=/home/user/build_output/xbot2_gui_client_x86_64 -DCMAKE_BUILD_TYPE=Release ../robot_monitoring
+~/Qt/$QT_VER/gcc_64/bin/qt-cmake -DCMAKE_INSTALL_PREFIX=/home/user/build_output/xbot2_gui_client_x86_64 -DCMAKE_BUILD_TYPE=Release ../robot_monitoring
 make install -j $(nproc)
 
 touch /home/user/build_output/BUILD_OK

c3460aa97811f50d9dadbf61c9381339200f5124
Arturo Laurenzi <arturo.laurenzi@iit.it>
2024-11-26 11:38:01 +0100

    dfl moto type for urdf

--- xbot2_gui/TestThings/MotionTab.js
+++ xbot2_gui/TestThings/MotionTab.js
@@ -20,7 +20,7 @@ function stop() {
 function connect() {
 
     let params = {
-        'motor_type': 'small_30',
+        'motor_type': 'CE0100A1',
         'load_mass': parseFloat(loadMassText.text),
         'load_radius': parseFloat(loadRadiusText.text)*0.01
     }

04e2cc18ffb8b0d0d39a40755789e216428c84d9
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-03-28 14:06:50 +0100

    Best effort joint states to avoid incompatible qos

--- server/src/xbot2_gui_server/joint_states.py
+++ server/src/xbot2_gui_server/joint_states.py
@@ -46,7 +46,7 @@ class JointStateHandler:
         self.srv.add_route('POST', '/joint_command/goto/stop', self.stop_handler, 'stop')
         
         # joint state subscriber
-        self.js_sub = ros_handle.create_subscription(JointState, 'xbotcore/joint_states', self.on_js_recv, queue_size=1)
+        self.js_sub = ros_handle.create_subscription(JointState, 'xbotcore/joint_states', self.on_js_recv, queue_size=1, best_effort=True)
         self.fault_sub = ros_handle.create_subscription(Fault, 'xbotcore/fault', self.on_fault_recv, queue_size=20)
         self.msg = None
         self.last_js_msg = None
